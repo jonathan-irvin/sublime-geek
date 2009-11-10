ebf70cfa90367c951db174cf6c196c794a7fc7eb
Jonathan Irvin <djfoxyslpr@gmail.com>
2009-11-09 21:49:35 -0600

    Added dashboard to index

--- cp/index.php
+++ cp/index.php
@@ -24,4 +24,79 @@ connect2slm();
 	</div>
 	<!--[if !IE]>end wrapper<![endif]-->
 
+	<!--[if !IE]>start section<![endif]-->
+				<div class="section">
+					
+					<h3>Dashboard</h3>
+					
+					<!--[if !IE]>start section content<![endif]-->
+					<div class="section_content">
+						
+						<!--[if !IE]>start lists<![endif]-->
+						<div class="lists">
+							<div class="lists_inner">
+								<dl>
+									<dt>MetaTip</dt>
+									<dd><a href="#">Store</a></dd>
+									<dd><a href="#">Shipping</a></dd>
+									<dd><a href="#">Authorize.net</a></dd>
+									<dd><a href="#">Emails</a></dd>
+									<dd><a href="#">Contact Form</a></dd>
+									<dd><a href="#">Lookbook</a></dd>
+									<dd><a href="#">SEO Settings</a></dd>
+									<dd><a href="#">Tax</a></dd>
+								</dl>
+								<dl>
+									<dt>MetaVotr</dt>
+									<dd><a href="#">Store</a></dd>
+									<dd><a href="#">Shipping</a></dd>
+									<dd><a href="#">Authorize.net</a></dd>
+									<dd><a href="#">Emails</a></dd>
+									<dd><a href="#">Contact Form</a></dd>
+									<dd><a href="#">Lookbook</a></dd>
+									<dd><a href="#">SEO Settings</a></dd>
+									<dd><a href="#">Tax</a></dd>
+								</dl>
+								<dl>
+									<dt>GridSplode</dt>
+									<dd><a href="#">Store</a></dd>
+									<dd><a href="#">Shipping</a></dd>
+									<dd><a href="#">Authorize.net</a></dd>
+									<dd><a href="#">Emails</a></dd>
+									<dd><a href="#">Contact Form</a></dd>
+									<dd><a href="#">Lookbook</a></dd>
+									<dd><a href="#">SEO Settings</a></dd>
+									<dd><a href="#">Tax</a></dd>
+								</dl>
+								<dl>
+									<dt>MetaStreamr</dt>
+									<dd><a href="#">Store</a></dd>
+									<dd><a href="#">Shipping</a></dd>
+									<dd><a href="#">Authorize.net</a></dd>
+									<dd><a href="#">Emails</a></dd>
+									<dd><a href="#">Contact Form</a></dd>
+									<dd><a href="#">Lookbook</a></dd>
+									<dd><a href="#">SEO Settings</a></dd>
+									<dd><a href="#">Tax</a></dd>
+								</dl>
+							</div>
+						</div>
+						<!--[if !IE]>end lists<![endif]-->
+						
+						<!--[if !IE]>start note<![endif]-->
+						<div class="note">
+							<dl>
+								<dt>Information</dt>
+								<dd>
+									With a couple of clicks you can manage all of your Sublime Geek products and get all the needed info here.
+								</dd>
+							</dl>
+						</div>
+						<!--[if !IE]>end note<![endif]-->
+						
+					</div>
+					<!--[if !IE]>end section content<![endif]-->
+				</div>
+				<!--[if !IE]>end section<![endif]-->
+				
 <? include "footer.php" ?>
