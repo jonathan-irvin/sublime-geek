376c097684478c5480d2ff1c1497e9862d9873c4
Jonathan Irvin <djfoxyslpr@gmail.com>
2009-11-09 21:38:32 -0600

    isolated footer and menu

--- cp/index.php
+++ cp/index.php
@@ -4,57 +4,8 @@ connect2slm();
 ?>
 
 <? include "header.php"; ?>
+<? include "menu.php"; ?>
 
-		<!--[if !IE]>start navs<![endif]-->
-		<div id="navs">
-			<!--[if !IE]>start main menu<![endif]-->
-			<div id="main_menu">
-				<div class="inner">
-					<ul>
-						<li class="sub selected">
-							<a href="#" class="selected"><span><span>Home</span></span></a>
-							<div class="submenu">
-								<ul>
-									<li><a href="#"><span><span>Settings</span></span></a></li>
-									<li><a href="#"><span><span>Orders</span></span></a></li>
-									<li><a href="#"><span><span>Catalog</span></span></a></li>
-									<li><a href="#"><span><span>Promotions</span></span></a></li>
-									<li><a href="#" class="selected"><span><span>Static Pages</span></span></a></li>
-									<li><a href="#"><span><span>Customer Services</span></span></a></li>
-								</ul>
-							</div>
-						</li>
-						<li class="sub">
-							<a href="#"><span><span>Settings</span></span></a>
-							<div class="submenu">
-								<ul>
-									<li><a href="#"><span><span>Settings</span></span></a></li>
-									<li><a href="#"><span><span>Orders</span></span></a></li>
-									<li><a href="#"><span><span>Catalog</span></span></a></li>
-									<li><a href="#"><span><span>Promotions</span></span></a></li>
-									<li><a href="#"><span><span>Static Pages</span></span></a></li>
-									<li><a href="#"><span><span>Customer Services</span></span></a></li>
-								</ul>
-							</div>
-						</li>
-						<li class="sub">
-							<a href="#"><span><span>Orders</span></span></a>
-							<div class="submenu">
-								<ul>
-									<li><a href="#"><span><span>Promotions</span></span></a></li>
-									<li><a href="#"><span><span>Static Pages</span></span></a></li>
-									<li><a href="#"><span><span>Customer Services</span></span></a></li>
-								</ul>
-							</div>
-						</li>
-						<li><a href="#"><span><span>Catalog</span></span></a></li>
-					</ul>
-				</div>
-			</div>
-			<!--[if !IE]>end main menu<![endif]-->
-			
-		</div>
-		<!--[if !IE]>end navs<![endif]-->
 		<!--[if !IE]>start content<![endif]-->
 		<div id="content">
 			<div class="inner">
@@ -72,16 +23,5 @@ connect2slm();
 		<!--[if !IE]>end content<![endif]-->
 	</div>
 	<!--[if !IE]>end wrapper<![endif]-->
-	<!--[if !IE]>start footer<![endif]-->
-	<div id="footer">
-		<div class="inner">
-			<p class="copyright">
-				<span class="app_details"><strong>Sublime Geek CP</strong> v2.0</span>
-				&copy; 2006-2009 Sublime Geek All rights reserved.
-			</p>
-			<a href="#" class="button scroll"><span><span>Go to top</span></span></a>
-		</div>	
-	</div>
-	<!--[if !IE]>end footer<![endif]-->
-</body>
-</html>
+
+<? include "footer.php" ?>
