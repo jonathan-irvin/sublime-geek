3e751afeaffc35b43a69331609a04695aed7ed68
Jonathan Irvin <djfoxyslpr@gmail.com>
2009-11-09 21:29:48 -0600

    isolated header into it's own file

---
+++ cp/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="imagetoolbar" content="no" />
+<title>Sublime Geek Control Panel</title>
+<link media="screen" rel="stylesheet" type="text/css" href="css/style.css"  />
+<!-- blue theme is default -->
+<link rel="stylesheet" type="text/css" href="css/black-theme.css" />
+<!--[if lte IE 6]><link media="screen" rel="stylesheet" type="text/css" href="css/ie.css" /><![endif]-->
+<script type="text/javascript" src="js/css.js"></script>
+<script type="text/javascript" src="js/behaviour.js"></script>
+</head>
