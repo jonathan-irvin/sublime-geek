5ff63ace8269596481f17ab55cd617b662e46be2
Jonathan Irvin <djfoxyslpr@gmail.com>
2009-11-29 21:54:18 -0600

    setup dashboard and login to communicate with each other

--- cp/system/application/controllers/dashboard.php
+++ cp/system/application/controllers/dashboard.php
@@ -6,37 +6,29 @@ class dashboard extends Controller {
 		parent::Controller();
 		$this->load->library('session');
 		$this->load->helper('form');
+		$this->load->helper('url');
 	}
 	
 	function index()
 	{
-		$session_id = $this->session->userdata('session_id');
-		$h_data = array('title' => 'Sublime Geek Control Panel - Please Login');
-		$this->load->view('login',$h_data);
-		
 		$user = $this->input->post('username', TRUE);
 		$pass = $this->input->post('password', TRUE);
 		
 		if($user && $pass)
 		{
+			$h_data = array(
+				   'title' => 'Sublime Geek Control Panel',
+				   'logo' => 'Sublime Geek <span>CP</span><em>v2.0</em>',
+				   'username' => 'Jon Desmoulins'               
+			  );		
 			
+			$this->load->view('header',$h_data);
+			$this->load->view('menu');
+			$this->load->view('index');
+			$this->load->view('footer');
+		}else{
+			redirect('/login/', 'refresh');
 		}
-		
-		$this->output->enable_profiler(TRUE);			
-	}
-	
-	function home()
-	{
-		$h_data = array(
-               'title' => 'Sublime Geek Control Panel',
-               'logo' => 'Sublime Geek <span>CP</span><em>v2.0</em>',
-               'username' => 'Jon Desmoulins'               
-          );		
-		
-		$this->load->view('header',$h_data);
-		$this->load->view('menu');
-		$this->load->view('index');
-		$this->load->view('footer');
 	}
 }
 ?>
--- cp/system/application/controllers/login.php
+++ cp/system/application/controllers/login.php
@@ -1,9 +1,20 @@
 <?php
-class Login extends Controller {
-
-	function index()
-	{	
-		$this->load->view('login');
+class login extends Controller {
+	
+	function login()
+	{
+		parent::Controller();
+		$this->load->library('session');
+		$this->load->helper('form');
+		$this->load->helper('url');
 	}
+	
+	function index()
+	{
+		$session_id = $this->session->userdata('session_id');
+		$h_data = array('title' => 'Sublime Geek Control Panel - Please Login');
+		$this->load->view('login',$h_data);
+		$this->output->enable_profiler(TRUE);			
+	}	
 }
 ?>
