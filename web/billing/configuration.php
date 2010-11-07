fdcd65788efe21d163957e7a8670fd66318e42d8
Jonathan Irvin <djfoxyslpr@gmail.com>
2010-11-07 12:08:52 -0600

    Removed billing.  Not needed at this time

--- web/billing/configuration.php
+++
@@ -1,9 +0,0 @@
-<?php
-$license = "Trial-41cbad33ec357ee64bdb";
-$db_host = "mysql50-64.wc2.dfw1.stabletransit.com";
-$db_username = "528093_whmcs";
-$db_password = "Jurb1f!ed";
-$db_name = "528093_whmcs";
-$cc_encryption_hash = "1CJRYLpfAN6OZSPkNemqFw3djXP2mkxeN6VBHaH7ODLDmrOZv0g1n94wWKo9UMex";
-$templates_compiledir = "templates_c/";
-?>
--- web/billing/templates_c/%%FA^FAE^FAE48E9E%%footer.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-10-03 20:48:18
-         compiled from v4/footer.tpl */ ?>
-    </div>
-  </div>
-  <div class="clear"></div>
-</div>
-<div id="footer">Copyright &copy; <a href="http://www.whmcs.com/" target="_blank">WHMCompleteSolution</a>.  All Rights Reserved.</div>
-</body>
-</html>
