eda469c32564f6ac863c955b1dbabaa94a62fce0
Jonathan Irvin <djfoxyslpr@gmail.com>
2011-08-14 05:08:59 -0500

    fixed a small routing issue

--- www.sblg.net/web/content/system/cms/config/config.php
+++ www.sblg.net/web/content/system/cms/config/config.php
@@ -12,7 +12,7 @@
 |
 */
 
-$config['base_url'] = '';
+$config['base_url'] = 'http://sblg.net/';
 
 /*
 |--------------------------------------------------------------------------
@@ -24,7 +24,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
