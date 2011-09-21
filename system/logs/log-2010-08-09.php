309682d17e532a349bea7464738ba5ef65b9d9ec
Jonathan Irvin <djfoxyslpr@gmail.com>
2011-09-20 19:01:57 -0500

    further committing changes

--- system/logs/log-2010-08-09.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2010-08-09 10:42:33 --> 404 Page Not Found --> popular/css
-ERROR - 2010-08-09 10:42:35 --> 404 Page Not Found --> popular/images
-ERROR - 2010-08-09 22:31:16 --> 404 Page Not Found --> font_swf
